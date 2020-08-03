1a080a029035c5662b421bcdec0ca998290c3179
Skillado <drumond.guilherme@hotmail.com>
2020-08-03 10:20:38 -0300

    ex1 start

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="_js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+            <div class="container-fluid">
+                
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" style="border:none;">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-center mx-auto">
+                              <li class="nav-item dropdown ml-2">
+                                    <a href="#" class="nav-link dropdown-toggle mr-5" data-toggle="dropdown" role="button" id="navDropdown" aria-haspopup="false" aria-expanded="false">Qual Exercicio?</a>
+                                    <div class="dropdown-menu bg-roxo-1 text-center" aria-labelledby="#navDropdown" style="border: none;">
+                                        <a href="pages/exs/ex1.php" class="dropdown-item  text-dark font-weight-light "  id="navDropdownF" aria-haspopup="true" aria-expanded="false">ex1</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownF" aria-haspopup="true" aria-expanded="false" >ex2</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex3</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex4</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex5</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex6</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex7</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex8</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex9</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex10</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex11</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex12</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex13</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex14</a>
+                                        <a href="#" class="dropdown-item  text-dark  font-weight-light " id="navDropdownM" aria-haspopup="true" aria-expanded="false">ex15</a>
+                                    </div>
+                              </li>
+                         </div>  
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <div class="landing">
+        <div class="home-wrap">
+            <div class="home-inner">
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="caption text-center ">
+        <h1 class="display-2 text-dark">Bem-Vind@ aos Exercicios Salesianos</h1>
+        <h4>Exercicios propostos pelo prof Daniel</h4>
+    </div>
+
+    <script type="text/JavaScript" src="js/bootstrap.bundle.min.js"></script>    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+</body>
+</html>
---
+++ pages/exs/ex1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="_js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+            <div class="container-fluid">
+                
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" style="border:none;"></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive"></div>
+            </div>
+        </nav>
+    </header>
+
+    
+
+    <div class="caption text-center ">
+        <h1 class="display-2 text-dark">Bem-Vind@ aos Exercicios Salesianos</h1>
+        <h4>Exercicios propostos pelo prof Daniel</h4>
+    </div>
+    <div class="container landing">
+        <div class="home-wrap">
+            <div class="home-inner d-flex justify-content-center">
+                <label for="ex1">Numero 1:</label>    
+                <input class="input" name="ex1" type="text"></input>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/JavaScript" src="js/bootstrap.bundle.min.js"></script>    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+</body>
+</html>
